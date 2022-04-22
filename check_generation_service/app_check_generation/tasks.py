04bf21c1fe786b109e699da86d1f373988227dea
Артур Токранов <artur.tokranov@gmil.com>
2022-04-22 19:32:05 +0300

    Делаю

--- check_generation_service/app_check_generation/tasks.py
+++ check_generation_service/app_check_generation/tasks.py
@@ -1,20 +1,49 @@
 import json
 import requests
-import os
-from check_generation_service.settings import BASE_DIR, MEDIA_ROOT
 import base64
+from django.template.loader import render_to_string
+import os
+import django
 
-url = 'http://localhost:49153/'
-html_file_path = os.path.join(BASE_DIR, 'templates', 'client_check.html')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE', 'check_generation_service.settings'
+)
+django.setup()
 
+url = 'http://127.0.0.1:55002/'
+
+context = {
+    "id": 123456,
+    "price": 780,
+    "items": [
+        {
+            "name": "Вкусная пицца",
+            "quantity": 2,
+            "unit_price": 250
+        },
+        {
+            "name": "Не менее вкусные роллы",
+            "quantity": 1,
+            "unit_price": 280
+        }
+    ],
+    "address": "г. Уфа, ул. Ленина, д. 42",
+    "client": {
+        "name": "Иван",
+        "phone": 9173332222
+    },
+    "point_id": 1
+}
+
+html = render_to_string('client_check.html', context=context)
+html = base64.b64encode(bytes(html, 'utf-8'))
 data = {
-    'contents': open(html_file_path, encoding='utf-8').read().encode('base64'),
+    'contents': str(html)[2:-1],
 }
 headers = {
-    'Content-Type': 'application/json',    # This is important
+    'Content-Type': 'application/json',
 }
 response = requests.post(url, data=json.dumps(data), headers=headers)
-print(response)
-# Save the response contents to a file
-# with open('file.pdf', 'wb') as f:
-#     f.write(response.content)
+
+with open('file.pdf', 'wb') as f:
+    f.write(response.content)
--- check_generation_service/check_generation_service/settings.py
+++ check_generation_service/check_generation_service/settings.py
@@ -39,7 +39,7 @@ ROOT_URLCONF = 'check_generation_service.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
