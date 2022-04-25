4871ebc5eda717a5a0c2f0290d85d09910745abb
Artur Tokranov <artur.tokranov@gmail.com>
2022-04-25 15:29:01 +0300

    Работаю над решением

--- check_generation_service/app_check_print/views.py
+++ check_generation_service/app_check_print/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+import os
+import requests
+import json
+from check_generation_service.settings import MEDIA_ROOT
 
-# Create your views here.
+url_new_checks = 'http://127.0.0.1:8000/new_checks/'
+url_check = 'http://127.0.0.1:8000/check/'
+api_key = 'key01'
+data = {
+    'api_key': api_key
+}
+headers = {'Content-Type': 'application/json'}
+
+response = requests.get(url_new_checks, data=json.dumps(data), headers=headers)
+data = response.json()
+
+if data.get('checks'):
+    id = data.get('checks')[0].get('id')
+    data = {
+        'api_key': api_key,
+        'check_id': id
+    }
+    response = requests.get(
+        url_check, data=json.dumps(data), headers=headers, stream=True
+    )
+    with open(os.path.join(MEDIA_ROOT, 'printing', 'file.pdf'), 'wb') as file:
+        file.write(response.content)
